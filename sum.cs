9504f52ead2d3fcdfdffbcee2c811ef205a4bed0
Sravani-03 <84711343+Sravani-03@users.noreply.github.com>
2022-02-01 10:27:27 +0530

    Add files via upload

---
+++ sum.cs
@@ -0,0 +1,28 @@
+﻿
+ 
+using System;
+ 
+namespace DemoSai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+         int i,sum=0;
+        Console.Write("input");
+        Console.Write("\n");
+        int n=int.Parse(Console.ReadLine());
+        for(i=1;i<=n;i++)
+        {
+            Console.Write("{0} ",i);
+            sum+=i;
+        }
+        Console.Write("\n");
+        Console.Write (sum); 
+    
+        
+        }
+        
+    }
+}
