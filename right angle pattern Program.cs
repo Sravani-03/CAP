d7498c6608eba3a4d985f921ff09ee150e783b29
Sravani-03 <84711343+Sravani-03@users.noreply.github.com>
2022-02-01 10:58:02 +0530

    Add files via upload

---
+++ right angle pattern Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Demosra
+{
+    internal class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            int i, j, r, k = 1;
+
+            Console.WriteLine("input");
+            Console.WriteLine("\n");
+
+            r = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= r; i++)
+            {
+                for (j = 1; j <= i; j++)
+                    Console.Write("{0} ", k++);
+
+                Console.WriteLine("\n");
+            }
+
+
+        }
+    }
+}
+  
