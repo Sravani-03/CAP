3068a65da6378cd6a11ac7c956442f07a86a7a7f
Sravani-03 <84711343+Sravani-03@users.noreply.github.com>
2022-02-02 09:55:42 +0530

    Add files via upload

---
+++ count a total number of alphabets, digits and special characters in a string.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Demosra
+{
+    class ADC
+      {
+        static void Main(string[] args)
+        {
+         string str;
+        int alphabet, digit, specialchar, i, l;
+        alphabet = digit = specialchar = i = 0;
+ 
+        Console.Write("string : ");
+        str = Console.ReadLine();
+        l = str.Length;
+ 
+        while (i < l)
+        {
+            if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            {
+                alphabet++;
+            }
+            else if (str[i] >= '0' && str[i] <= '9')
+            {
+                digit++;
+            }
+            else
+            {
+                specialchar++;
+            }
+ 
+            i++;
+        }
+ 
+        Console.Write("Alphabets: {0}\n", alphabet);
+        Console.Write("Digits : {0}\n", digit);
+        Console.Write("Special characters: {0}\n\n", specialchar);
+ 
+        Console.ReadLine();
+        }
+    }
+}
+  
