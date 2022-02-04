a9ecf0651868bd5891e24618e4c957ac9592ab27
Sravani-03 <84711343+Sravani-03@users.noreply.github.com>
2022-02-04 16:16:33 +0530

    Add files via upload

---
+++ structure.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Demosra
+{
+     
+    public struct Student {
+    public string Name;
+    public int Class;
+    public string Gender;
+    }
+    class Vstruct
+    {
+        public static void Main(string[] args)
+        {
+
+        Student S1;
+        S1.Name = "Sravani";
+        S1.Class = 5;
+        S1.Gender = "F";
+
+        Console.WriteLine("Name : {0}", S1.Name);
+        Console.WriteLine("Class: {0}",S1.Class);
+        Console.WriteLine("Gender : {0}",S1.Gender);
+
+        }
+    }
+}
