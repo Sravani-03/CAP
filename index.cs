9161c993d6d70babe43fb18ef1c3299017cf605a
Sravani-03 <84711343+Sravani-03@users.noreply.github.com>
2022-02-04 16:15:55 +0530

    Add files via upload

---
+++ index.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Demosra
+{
+     
+    public class IndexCreation 
+    {
+        private string[] val = new string[3];
+        public string this[int index]{
+            get
+        {
+ 
+            return val[index];
+        }
+        set
+        {
+            val[index] = value;
+        }
+        }
+    
+    }
+    class Indexersinc
+    {
+        public static void Main(string[] args)
+        {
+
+       IndexCreation ic = new IndexCreation();
+        ic[0] = "JAVA";
+        ic[1] = "C#";
+        ic[2] = "Python";
+
+        Console.WriteLine("first: {0}", ic[0]);
+        Console.WriteLine("Second: {0}",ic[1]);
+        Console.WriteLine("third : {0}",ic[2]);
+
+        }
+    }
+}
